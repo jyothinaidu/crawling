b8566bab397b5473afb386a9c2b91e94bfb6d8ad
jyothinaidu <jyothi.nadu@gmail.com>
2017-02-11 23:46:22 +0530

    Commit

--- crawling/settings.py
+++ crawling/settings.py
@@ -80,13 +80,25 @@ WSGI_APPLICATION = 'crawling.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hello',
+        'USER': 'hello_django',
+        'PASSWORD': 'Sahi@2010',
+        'HOST': 'localhost',
+        'PORT': '',                      # Set to empty string for default.
+    }
+}
+
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),)
